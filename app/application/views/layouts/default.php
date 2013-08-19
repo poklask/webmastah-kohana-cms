24e95fcc601adf535764881b0707f74a61b87f8f
sbl <sbl@invedia.pl>
2013-08-19 14:35:41 +0200

    poprawki + przygotowanie do nowego wpisu

--- app/application/views/layouts/default.php
+++ app/application/views/layouts/default.php
@@ -5,30 +5,35 @@
 	<title>Webmastah.pl - Kohana CMS</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="//code.jquery.com/jquery.js"></script>
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css">
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-wip/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//getbootstrap.com/examples/justified-nav/justified-nav.css">
+	<link href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,300italic,400italic,500,500italic,900,900italic,700,700italic" rel="stylesheet" type="text/css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-wip/js/bootstrap.min.js"></script>
 	<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<style>
+	body{font-family: Roboto, Helvetica, Sans !important;}
+	</style>
 </head>
 <body>
 	<div class="container">
-		<div class="navbar">
-			<a class="navbar-brand" href="<?php echo url::base() ?>">Webmastah</a>
-			<ul class="nav navbar-nav">
+		<div class="masthead">
+			<h3 class="text-muted">Webmastah.pl <span class="pull-right" style="font-size:12px; font-weight:500;color:#333;"><a href="<?php echo url::site('konto/logowanie') ?>">Zaloguj się</a> | <a href="<?php echo url::site('konto/rejestracja') ?>">Utwórz konto</a></span></h3>
+			<ul class="nav nav-justified">
 				<li><a href="<?php echo url::site('/') ?>">Home</a></li>
-				<li><a href="<?php echo url::site('/kim-jestesmy') ?>">Kim jestem</a></li>
+				<li><a href="<?php echo url::site('/kim-jestesmy') ?>">Kim jesteśmy</a></li>
 				<li><a href="<?php echo url::site('/aktualnosci') ?>">Aktualności</a></li>
 				<li><a href="<?php echo url::site('/galeria') ?>">Galeria</a></li>
 				<li><a href="<?php echo url::site('/konto') ?>">Moje konto</a></li>
 			</ul>
 		</div>
-		
+
 		<div class="row-fluid">
 		<?php echo $content ?>
 		</div>
 
 		<hr>
 		<div class="footer">
-		<p>&copy; 2013 TwojaNazwa</p>
+		<p>Copyright &copy; 2013 Webmastah.pl | <a href="http://opensource.org/licenses/MIT" target="_blank">MIT License</a></p>
 		</div>
 	</div> 
 </body>
