2ecd0e330b59ead4812242464a869371f8e948bb
sbl <sbl@invedia.pl>
2013-08-19 14:48:02 +0200

    poprawki

--- index.php
+++ index.php
@@ -8,29 +8,22 @@ define('EXT', '.php');
 
 error_reporting(E_ALL | E_STRICT);
 
-// Set the full path to the docroot
 define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 
-// Make the application relative to the docroot, for symlink'd index.php
 if ( ! is_dir($application) AND is_dir(DOCROOT.$application))
 	$application = DOCROOT.$application;
 
-// Make the modules relative to the docroot, for symlink'd index.php
 if ( ! is_dir($modules) AND is_dir(DOCROOT.$modules))
 	$modules = DOCROOT.$modules;
 
-// Make the system relative to the docroot, for symlink'd index.php
 if ( ! is_dir($system) AND is_dir(DOCROOT.$system))
 	$system = DOCROOT.$system;
 
-// Define the absolute paths for configured directories
 define('APPPATH', realpath($application).DIRECTORY_SEPARATOR);
 define('MODPATH', realpath($modules).DIRECTORY_SEPARATOR);
 define('SYSPATH', realpath($system).DIRECTORY_SEPARATOR);
 
-// Clean up the configuration vars
 unset($application, $modules, $system, $app);
-
 
 if ( ! defined('KOHANA_START_TIME'))
 {
@@ -42,7 +35,6 @@ if ( ! defined('KOHANA_START_MEMORY'))
 	define('KOHANA_START_MEMORY', memory_get_usage());
 }
 
-// Bootstrap the application
 require APPPATH.'bootstrap'.EXT;
 
 if (PHP_SAPI == 'cli') // Try and load minion
